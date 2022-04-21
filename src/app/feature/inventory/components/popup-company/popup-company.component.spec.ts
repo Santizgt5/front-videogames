036a772ea27f41ac60450f7568fba1a3556d1f66
Santiago Alvarez Rivera - Ceiba Software <santiago.alvarez@ceiba.com.co>
2022-04-20 23:22:56 -0500

    Fix test

--- src/app/feature/inventory/components/popup-company/popup-company.component.spec.ts
+++ src/app/feature/inventory/components/popup-company/popup-company.component.spec.ts
@@ -1,62 +1,64 @@
-// import { HttpClientModule } from '@angular/common/http';
-// import { ComponentFixture, TestBed} from '@angular/core/testing';
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { MatNativeDateModule } from '@angular/material/core';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatDialogRef } from '@angular/material/dialog';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatSelectModule } from '@angular/material/select';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { CompanyService } from '@inventory/shared/service/company.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Company } from '@inventory/shared/model/company';
+import { CompanyService } from '@inventory/shared/service/company.service';
+import Swal from 'sweetalert2';
 
-// import { PopupCompanyComponent } from './popup-company.component';
+import { PopupCompanyComponent } from './popup-company.component';
 
-// describe('PopupCompanyComponent', () => {
-//   let component: PopupCompanyComponent;
-//   let element;
-//   let fixture: ComponentFixture<PopupCompanyComponent>;
-//   let mockCompanyService: any;
+describe('PopupCompanyComponent', () => {
+  let component: PopupCompanyComponent;
+  let element;
+  let fixture: ComponentFixture<PopupCompanyComponent>;
+  let mockCompanyService: any;
 
-//   beforeEach(async () => {
-//     mockCompanyService = jasmine.createSpyObj<CompanyService>('serviceMockCompany', ['createCompany'],);
-//     await TestBed.configureTestingModule({
-//       declarations: [ PopupCompanyComponent ],
-//       providers: [ { provide: MatDialogRef, useValue: { close: () => {} }},
-//      { provide: CompanyService, useValue: mockCompanyService} ],
-//      imports: [ ReactiveFormsModule, HttpClientModule,  MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, BrowserAnimationsModule]
-//     })
-//     .compileComponents();
-//   });
+  beforeEach(async () => {
+    mockCompanyService = jasmine.createSpyObj<CompanyService>('serviceMockCompany', ['createCompany'],);
+    await TestBed.configureTestingModule({
+      declarations: [ PopupCompanyComponent ],
+      providers: [ { provide: MatDialogRef, useValue: { close: () => {} }},
+     { provide: CompanyService, useValue: mockCompanyService} ],
+     imports: [ ReactiveFormsModule, HttpClientModule,  MatFormFieldModule, MatInputModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, BrowserAnimationsModule]
+    })
+    .compileComponents();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PopupCompanyComponent);
-//     component = fixture.componentInstance;
-//     element = fixture.debugElement.nativeElement;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PopupCompanyComponent);
+    component = fixture.componentInstance;
+    element = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+  });
 
-//   afterEach(() => {
-//     document.body.removeChild(element);
-//   })
+  afterEach(() => {
+    document.body.removeChild(element);
+  })
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it('should create company', fakeAsync(() => {
-  //   const data: Company =  {
-  //     "name": "Bungie",
-  //     "description": "Ejemplo de descripción",
-  //     "born": "2022-03-08",
-  //     "nit": "231231"
-  // }
-  //   const ret = {"valor": 2} 
-  //   component.formGroup.setValue(data);
-  //   mockCompanyService.createCompany.and.returnValue(ret);
-  //   component.crearDeveloper();
-  //   tick(5000);
-//   //   expect(Swal.isVisible()).toBeTruthy();
-//   // }));
+  it('should create company', fakeAsync(() => {
+    const data: Company =  {
+      "name": "Bungie",
+      "description": "Ejemplo de descripción",
+      "born": "2022-03-08",
+      "nit": "231231"
+  }
+    const ret = {"valor": 2} 
+    component.formGroup.setValue(data);
+    mockCompanyService.createCompany.and.returnValue(ret);
+    component.crearDeveloper();
+    tick(5000);
+    expect(Swal.isVisible()).toBeTruthy();
+  }));
 
-// });
+});
