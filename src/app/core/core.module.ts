de639f415463d184458a80b7e57f573eeb8959d1
Santiago Alvarez Rivera - Ceiba Software <santiago.alvarez@ceiba.com.co>
2022-04-22 07:39:22 -0500

    Delete project base ceiba

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SecurityGuard } from './guard/security.guard';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptor } from './interceptor/token-interceptor';
-import { AuthInterceptor } from './interceptor/auth-interceptor';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpService } from './services/http.service';
 import { ManejadorError } from './interceptor/manejador-error';
@@ -22,7 +21,6 @@ import { MatIconModule } from '@angular/material/icon';
     HttpService,
     SecurityGuard,
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: ErrorHandler, useClass: ManejadorError }
   ]
 })
